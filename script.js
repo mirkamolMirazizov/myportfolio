d46ec2a7e23f80f9df0cd00e35a840c1e2390e17
miraz <miraz@MIRKAMOL>
2024-10-16 17:53:51 -0400

    edited version

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function toggleDetails(id) {
-    var element = document.getElementById(id);
-    element.classList.toggle('hidden');
-}
+// Back to Top Button
+const backToTopBtn = document.getElementById('backToTopBtn');
+
+// Show the button when scrolling down
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopBtn.classList.remove('hidden');
+    } else {
+        backToTopBtn.classList.add('hidden');
+    }
+};
+
+// Smooth Scroll Back to Top
+backToTopBtn.addEventListener('click', function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
